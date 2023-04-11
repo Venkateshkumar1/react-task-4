f8417cd9db20d0292165055f8547de6afc8419ae
Venkateshkumar1 <kishorkani4321@gmail.com>
2023-04-11 11:15:52 +0530

    edit

---
+++ src/OverviewChart.js
@@ -0,0 +1,14 @@
+import { EarningsOverview } from './earningOverview';
+
+export function OverviewChart() {
+    return (
+        <div className='overview-chart'>
+            <div className='overview-head'>
+                <h4>Earnings Overview</h4>
+            </div>
+            <div style={{ height: '350px', padding: '0px 15px' }}>
+                <EarningsOverview />
+            </div>
+        </div>
+    );
+}
